eff0d2d61a6586fdf32e8fbc7a2a560f82f509cf
zineb19971231 <zinebakarras@gmail.com>
2026-04-07 14:42:04 +0100

    add files model controller view register

--- App/controllers/Authcontroller.php
+++ App/controllers/Authcontroller.php
@@ -7,9 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $database = new Database();
     $db = $database->getConnection();
-
     $user = new User($db);
-
     // Get form  the data
     $user->setUsername($_POST['username']);
     $user->setEmail($_POST['email']);
