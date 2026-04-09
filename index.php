23a5b27b044eca8f1bc05eb14e53ac2f4bc909c8
zineb19971231 <zinebakarras@gmail.com>
2026-04-09 20:08:47 +0100

    add style file , delete public folder , add logic index

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require_once "config/db.php"; 
+require_once "App/models/User.php";
+require_once "App/controllers/AuthController.php";
+$page = $_GET['page'] ?? 'home';
+$database = new Database();
+$db = $database->getConnection();
+$auth = new AuthController($db);
+
+switch ($page) {
+    case 'login':
+        $auth->handleLogin();
+        include 'App/views/Auth/login.php'; 
+        break;
+    case 'register':
+        $auth->handleRegister();
+        include 'App/views/Auth/register.php'; 
+
+        break;
+    case 'dashboard':
+        include 'App/views/DashboardUser.php';
+        break;
+    default:
+        echo "<h1>Bienvenue</h1><a href='?page=login'>Se connecter</a> | <a href='?page=register'>S'inscrire</a>";
+}
+?>
