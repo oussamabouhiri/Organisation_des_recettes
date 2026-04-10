5bbd88d6b23dfc6c6d99f5230f58fdac9e7736b4
zineb19971231 <zinebakarras@gmail.com>
2026-04-10 11:37:32 +0100

    add files to branch feature/lister_recettes

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 require_once "config/db.php"; 
 require_once "App/models/User.php";
 require_once "App/controllers/AuthController.php";
+require_once "App/controllers/Recettecontroller.php";
+
 $page = $_GET['page'] ?? 'home';
 $database = new Database();
 $db = $database->getConnection();
 $auth = new AuthController($db);
-$recipeModel = new Recette($db);
+$recetteController = new Recettecontroller($db);
 $recettes = $recipeModel->getAllRecettes();
 
 switch ($page) {
@@ -14,14 +16,15 @@ switch ($page) {
         $auth->handleLogin();
         include 'App/views/Auth/login.php'; 
         break;
+
     case 'register':
         $auth->handleRegister();
         include 'App/views/Auth/register.php'; 
+        break;
 
-        break;
-    case 'dashboard':
-        include 'App/views/DashboardUser.php';
-        break;
+   case 'acceuil':
+            $recetteController->listRecette();
+            break;
     default:
         echo "<h1>Bienvenue</h1><a href='?page=login'>Se connecter</a> | <a href='?page=register'>S'inscrire</a>";
 }
